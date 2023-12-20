fe6bafcfda510e46c69690400129664492ed2809
alexf <alexfurman20@gmail.com>
2023-12-20 19:27:07 +0200

    Home work 1 module

---
+++ first_module_1.py
@@ -0,0 +1,8 @@
+age = 10
+weight = 67
+print("Human age =", age)
+print(f"Human age = {age} and weight = {weight}")
+print("Human age =  {} and weight = {}".format(age, weight))
+
+print(age, weight, sep="#", end="     ")
+print("Hello")
