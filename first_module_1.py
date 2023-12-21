9207be3245de0e7ef75d449273054726c5035d48
AlexFurman89 <49728152+AlexFurman89@users.noreply.github.com>
2023-12-21 23:27:25 +0200

    Update first_module_1.py

--- first_module_1.py
+++ first_module_1.py
@@ -6,3 +6,11 @@ print("Human age =  {} and weight = {}".format(age, weight))
 
 print(age, weight, sep="#", end="     ")
 print("Hello")
+
+###
+
+name = input("Enter name\n")
+age = int(input("Enter age\n"))
+weight = float(input("Enter you weight\n"))
+age = age + 1
+print("Hello,",name, "You are",age,"yeare old and you weight is", weight)
