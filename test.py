7878f2e618877c773a8761c27438b4312d930c28
alexf <alexfurman20@gmail.com>
2024-01-27 09:59:25 +0200

    edited

---
+++ test.py
@@ -0,0 +1,55 @@
+# array=[1,2,3,4]
+# # print(array.pop(2))
+# # print(array)
+# # array.append(5)
+# # print(array[1])
+# array_2=[5,6]
+# # print(array+array_2)
+# array_3=array_2
+# array_3=array_2[:]
+# array_3.append(10)
+# print(array_2)
+
+# var ={
+# 'a':'abc',
+# 'price':123
+# }
+# var_2 ={
+# 'price':123,
+# 'a':'abc'
+# }
+# print(var==var_2)
+# print(id(var))
+# print(id(var_2))
+# print(id(var)==id(var_2))
+# print(type(var))
+
+# var=[4,3,7,2,1]
+# # var.sort(reverse=True)
+# var.sort(reverse=True)
+# var.append(102)
+# var.pop(-2)
+# print(var)
+
+
+# var= {
+#     'brand':'Nike',
+#     'price':1000,
+#     'currency':'uah',
+#     'contact_info':{
+#         'Name':'Alex',
+#         'Surname':'Furman'
+#     }
+#     }
+# var['phone']='None'
+# del var['phone']
+# print(var)
+# var.pop['brand']
+# print(var)
+# print(var['cell'])
+# print(var.get('cell'))
+# print(var.get('cell','not exist'))
+
+
+var = {}
+print(var)
--- third_module_1.py
+++ third_module_1.py
@@ -5,7 +5,7 @@ numberOfDigits = int(input("Enter number of digits you want to fill in: "))
 array = []
 sum = 0
 for i in range(numberOfDigits):
-    print(i+1, end=" ")
-    array.append(int(input("Number is: ")))
+    # print(i+1, end=" ")
+    array.append(int(input(f"{i+1} Number is: ")))
     sum += array[i]
 print(sum)
