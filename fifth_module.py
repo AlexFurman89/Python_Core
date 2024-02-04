dba7f6549964437a256d408b2a64ab9c48b98e16
alexf <alexfurman20@gmail.com>
2024-02-04 19:41:56 +0200

    edit

--- fifth_module.py
+++ fifth_module.py
@@ -46,12 +46,11 @@ def edit_by_field(field, company):
             if old_id in employee:
                 flag = 1
         if flag == 1:
+            new_id = int(input("Enter a new id: "))
+            flag_2 = 1
             for employee in company:
-                new_id = int(input("Enter a new id: "))
-                flag_2 = 1
-                for employee in company:
-                    if new_id in employee:
-                        flag_2 = 0
+                if new_id in employee:
+                    flag_2 = 0
                 if flag_2 == 1:
                     employee[1] = new_id
                 elif flag_2 == 0:
